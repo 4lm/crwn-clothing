d3d3138bf695895fa4f0c5347cd53cacf2f960b7
Alexis Michaltsis <alexis@michaltsis.net>
2019-08-03 13:25:06 +0200

    Put non-reusable components into pages folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './components/homepage/Homepage';
+import Homepage from './components/pages/homepage/Homepage';
 
 function App() {
   return (
