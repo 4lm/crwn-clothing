fda7806680026eac5ca02985120f7956cd38963c
Alexis Michaltsis <alexis@michaltsis.net>
2019-08-04 15:57:17 +0200

    Add collection item and collection item styling

---
+++ src/components/collection/item/Item.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Item.scss';
+
+const Item = ({ id, name, price, imageUrl }) => (
+  <div className="item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default Item;
--- src/components/collection/preview/Preview.jsx
+++ src/components/collection/preview/Preview.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import './Preview.scss';
 
+import CollectionItem from 'components/collection/item/Item';
+
 const Preview = ({ title, items }) => (
   <div className="collection">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((_, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherProps }) => (
+          <CollectionItem key={id} {...otherProps} />
         ))}
     </div>
   </div>
