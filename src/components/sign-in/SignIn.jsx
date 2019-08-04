5c01cff770260ebf0e138d5245818c74c36b25d9
Alexis Michaltsis <alexis@michaltsis.net>
2019-08-04 20:55:20 +0200

    Add form input component

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+import './SignIn.scss';
+
+import 'components/form-input/FormInput';
+import FormInput from 'components/form-input/FormInput';
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -25,20 +30,20 @@ class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='email'
             required />
-          <label>Email</label>
-          <input
+          <FormInput
             name='password'
             type='password'
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='password'
             required />
-          <label>Password</label>
           <input type="submit" value="Submit Form" />
         </form>
       </div>
