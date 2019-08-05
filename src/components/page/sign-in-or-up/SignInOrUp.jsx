e7af12e2a0f40a0f93c68798608fd7edb7074ea6
Alexis Michaltsis <alexis@michaltsis.net>
2019-08-05 16:27:31 +0200

    Implement sing up component into sing in or up page

--- src/components/page/sign-in-or-up/SignInOrUp.jsx
+++ src/components/page/sign-in-or-up/SignInOrUp.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './SignInOrUp.scss';
 
 import SignIn from 'components/sign-in/SignIn';
+import SignUp from 'components/sign-up/SignUp';
 
 const SignInOrUp = () => (
-  <div className="sign-in">
-    <SignIn />  
+  <div className="sign-in-or-up">
+    <SignIn />
+    <SignUp />
   </div>
 );
 
