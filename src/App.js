29fd818a67bd5d522bf4d865a7d589c2ed5cabb5
Alexis Michaltsis <alexis@michaltsis.net>
2019-08-05 13:52:41 +0200

    Observe auth state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -8,17 +8,41 @@ import PageHome from 'components/page/home/Home';
 import PageShop from 'components/page/shop/Shop';
 import PageSignInOrUp from 'components/page/sign-in-or-up/SignInOrUp';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={PageHome} />
-        <Route path='/shop' component={PageShop} />
-        <Route path='/sign-in' component={PageSignInOrUp} />
-      </Switch>
-    </div>
-  );
+import { auth } from 'firebase/utils';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={PageHome} />
+          <Route path='/shop' component={PageShop} />
+          <Route path='/sign-in' component={PageSignInOrUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
